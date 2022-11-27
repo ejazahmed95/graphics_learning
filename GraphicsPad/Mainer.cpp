bb7e07771b89943b37c91b8d7d54f632f55919cf
Ejaz Ahmed <ejaz.mohammed@utah.edu>
2022-11-27 10:58:47 -0700

    Added stb image loader

--- GraphicsPad/Mainer.cpp
+++ GraphicsPad/Mainer.cpp
@@ -1,6 +1,9 @@
 #include <Qt\qapplication.h>
 #include "GLWindow.h"
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
--- GraphicsPad/src/GLWindow.cpp
+++ GraphicsPad/src/GLWindow.cpp
@@ -5,6 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <stb_image.h>
 
 #include <iostream>
 
@@ -95,6 +96,31 @@ void GLWindow::sendData() {
 	glBindVertexArray(planeVAO); {
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufferID);
 		setShapeData(plane);
+		
+		GLuint texId;
+		glGenTextures(1, &texId);
+		glBindTexture(GL_TEXTURE_2D, texId);
+
+		// set the texture wrapping/filtering options (on the currently bound texture object)
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		// load and generate the texture
+		int width, height, nrChannels;
+		unsigned char* data = stbi_load("resources/textures/wall.jpg", &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
+		}
+		else
+		{
+			std::cout << "Failed to load texture" << std::endl;
+		}
+		stbi_image_free(data);
+
 	} glBindVertexArray(0);
 
 	// Cube
@@ -120,17 +146,6 @@ void GLWindow::sendData() {
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, bufferID);
 		setShapeData(teapot);
 	} glBindVertexArray(0);
-
-	/*
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load("resource/textures/wall.jpg", &width, &height, &nrChannels, 0);
-
-	GLuint texId;
-	glGenTextures(1, &texId);
-	glBindTexture(GL_TEXTURE_2D, texId);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	*/
 	
 }
 
